52a8ff3ca2a4d56558cf5e4b66b421497803ce66
nampallysirisha <148890197+nampallysirisha@users.noreply.github.com>
2023-10-24 22:20:22 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#create a calculator
+a=int(input("enter any number:"))
+b=int(input("enter any number:"))
+print("addition=",a+b)
+print("subtraction=",a-b)
+print("multiplication=",a*b)
+print("division=",a/b)
+print("remainder=",a%b)
+print("floor division value=",a//b)
